fffb8f9055c88508e81f61c4bfb861533ab6e911
vishant002 <vishantagrawal2002@gmail.com>
2023-06-09 17:23:47 +0530

    adding dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath : '/bookmarker/',
   transpileDependencies: true
 });
 // publicPath : "Bookmarker-App"
